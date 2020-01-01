e1a358d2335092c413f2cab8a7b26a3573b18da7
Uganda-Knuckles <59398982+Uganda-Knuckles@users.noreply.github.com>
2020-01-01 14:13:24 -0300

    Update Transparent base.js

--- Scripts/Transparent base.js
+++ Scripts/Transparent base.js
@@ -13,13 +13,3 @@ window.renderPlayer = function(a, d, c, b, g) {
   a.skin && skinSprites[a.skin] && skinSprites[a.skin].readyToDraw ? (e = a.size - b.lineWidth / 4, b.lineWidth /= 2, renderCircle(d, c, a.size, b, !1, !0)) : g || (b.fillStyle = "rgba(255, 255, 255, 0)", renderCircle(d, c, a.size, b));
   b.restore();
 };
-function moveSelUnits() {
-  if (selUnits.length) {
-    var a = player.x + targetDst * MathCOS(targetDir) + camX, d = player.y + targetDst * MathSIN(targetDir) + camY;
-    for (var e = [], b = 0; b < selUnits.length; ++b) {
-      e.push(selUnits[b].id);
-    }
-    socket.emit("5", UTILS.roundToTwo(a), UTILS.roundToTwo(d), e, 0, -1);
-  }
-}
-;
