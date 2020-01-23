f3cb1f41cb3f60aede12e1a698fdf14e8d07e70b
Uganda-Knuckles <59398982+Uganda-Knuckles@users.noreply.github.com>
2020-01-23 20:49:59 -0300

    Update game.js

--- Scripts/Custom Skin/game.js
+++ Scripts/Custom Skin/game.js
@@ -1,1 +1,18 @@
+function httpGetAsync(theUrl, callback) { //theURL or a path to file
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var data = httpRequest.responseText;  //if you fetch a file you can JSON.parse(httpRequest.responseText)
+            if (callback) {
+                callback(data);
+            }                   
+        }
+    };
 
+    httpRequest.open('GET', theUrl, true); 
+    httpRequest.send(null);
+}
+
+httpGetAsync('http://..... or fetch a file', function(data) {
+    //do something with your data
+});
